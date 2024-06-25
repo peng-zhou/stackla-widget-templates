f552779bbc40442e76e48e204b5573698a77440f
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-06-26 08:53:40 +1000

    Add support for new esbuild pipeline, cleanup typescript

---
+++ esbuild.js
@@ -0,0 +1,17 @@
+import * as esbuild from 'esbuild'
+
+const widgets = [
+  'example'
+]
+
+await esbuild.build({
+  entryPoints: [
+    ...widgets.map(widget => `widgets/${widget}/widget.ts`)
+  ],
+  bundle: true,
+  outdir: 'dist',
+  loader: {
+    '.hbs': 'text',
+    '.css': 'text'
+  }
+});
