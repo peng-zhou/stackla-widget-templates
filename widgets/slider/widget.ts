c126418e063b684e782b96b48363e132421159cf
peng-zhou <peterzhou.syd@gmail.com>
2024-07-18 14:44:47 +1000

    slider widget template

--- widgets/slider/widget.ts
+++ widgets/slider/widget.ts
@@ -40,9 +40,41 @@ addCSSVariablesToPlacement(getCSSVariables());
 addAutoAddTileFeature<IWidgetSettings>(widgetSettings);
 loadExpandedTileFeature(widgetSettings);
 addTilesPerPageFeature<IWidgetSettings>(widgetSettings);
-addLoadMoreButtonFeature<IWidgetSettings>(widgetSettings);
 
-sdk.addEventListener("load", () => initializeMasonry());
+sdk.addEventListener("load", () => {
+
+  const sliderScrollUpButton = sdk.querySelector("#scrollUp");
+  const slidewrScrollDownButton = sdk.querySelector("#scrollDown");
+  const tileBlockElement = sdk.querySelector(".ugc-tile-wrapper");
+  const tilesContainer = sdk.querySelector(".ugc-tiles");
+
+  const blockHeight = tileBlockElement?.offsetHeight ? tileBlockElement.offsetHeight : '220';
+
+  console.log("slider blockHeight", blockHeight);
+  console.log("slider sliderScrollUpButton", sliderScrollUpButton);
+  console.log("slider tileBlockElement", tileBlockElement);
+  console.log("slider tilesContainer", tilesContainer);
+  if (sliderScrollUpButton && blockHeight !== undefined) {
+    sliderScrollUpButton.addEventListener("click", () => {
+      console.error("sliderScrollUpButton clicked", blockHeight);
+      if (tilesContainer && blockHeight !== undefined) {
+        tilesContainer.scrollBy({
+          top: -blockHeight,
+          behavior: "smooth",
+        });
+      } else if (!tilesContainer) {
+        console.error("Slider Tiles Scroll Container not found");
+      }
+    });
+  } else {
+    if (!sliderScrollUpButton) {
+      console.error("Slider Tiles Scroll Up Button not found");
+    }
+    if (blockHeight === undefined) {
+      console.error("Slider Tile not found or has no height");
+    }
+  }
+});
 sdk.addEventListener("moreLoad", () => loadMoreMasonryTiles());
 sdk.addEventListener("tilesUpdated", () => refreshMasonryLayout());
 sdk.addCSSToComponent(expandedTileCSS, "expanded-tile");
