aba25c8eb9304630a2be71aa648ce96bad9f2a78
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-07-30 15:23:27 +1000

    Fixup ts

--- widgets/waterfall/widget.config.ts
+++ widgets/waterfall/widget.config.ts
@@ -57,7 +57,8 @@ export function getConfig(widgetContainer: WidgetContainer): IWidgetSettings {
     expanded_tile_show_caption: widgetConfig.lightbox.show_caption ?? true,
     expanded_tile_show_shopspots: widgetConfig.lightbox.show_shopspots ?? false,
     expanded_tile_show_timestamp: widgetConfig.lightbox.show_timestamp ?? true,
-    expanded_tile_show_add_to_cart: widgetConfig.lightbox.show_add_to_cart ?? false
+    expanded_tile_show_add_to_cart: widgetConfig.lightbox.show_add_to_cart ?? false,
+    expanded_tile_show_tags: widgetConfig?.lightbox?.show_tags ?? false
   }
 
   if (!enabled) {
