b97cd98ee4177d300ebc405996b8857ed2b8bb5e
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-07-30 17:22:55 +1000

    Remove comments

--- widgets/libs/extensions/masonry.d.ts
+++ widgets/libs/extensions/masonry.d.ts
@@ -23,24 +23,20 @@ declare module "masonry-layout" {
         masonry(): void;
         masonry(eventName: string, listener: any): void;
 
-        // layout
         layout(): void;
         layoutItems(items: any[], isStill?: boolean): void;
         stamp(elements: any[]): void;
         unstamp(elements: any[]): void;
 
-        // add and remove items
         appended(elements: any[]): void;
         prepended(elements: any[]): void;
         addItems(elements: any[]): void;
         remove(elements: any[]): void;
 
-        // events
         on(eventName: string, listener: any): void;
         off(eventName: string, listener: any): void;
         once(eventName: string, listener: any): void;
 
-        // utilities
         reloadItems(): void;
         destroy(): void;
         getItemElements(): any[];
