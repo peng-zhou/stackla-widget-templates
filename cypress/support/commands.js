82e6dda804fd172b6cfea71b4ec563421fc5a067
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-08-13 11:24:38 +1000

    Add tests

---
+++ cypress/support/commands.js
@@ -0,0 +1,41 @@
+Cypress.Commands.add('visitWidget', (widgetType) => {
+    cy.visit(`http://localhost:4002/preview?widgetType=${widgetType}`);
+});
+
+Cypress.Commands.add('widgetTests', (widgetType) => {
+    cy.visitWidget(widgetType);
+
+    const widget = cy.get('ugc-widget-668ca52ada8fb');
+    const shadowRoot = widget.shadow();
+
+    it('should show ugc-tile', () => {
+        widget.should('exist');
+        shadowRoot
+            .find('.ugc-tile')
+            .should('have.length', 100);
+    })
+
+    it('should expand tile when clicked', () => {
+        shadowRoot
+            .find('.ugc-tile')
+            .first()
+            .click();
+
+        const expandedTile = shadowRoot.get('expanded-tile');
+
+        expandedTile.should('exist')
+
+        const expandedTileShadow = expandedTile.shadow();
+
+        expandedTileShadow
+            .find('img')
+            .should('exist');
+
+        expandedTileShadow.find('.stacklapopup-products-item-title').should('exist');
+
+        expandedTileShadow
+            .find('.stacklapopup-products-item-title')
+            .invoke('text')
+            .should('not.be.empty');
+    });
+});
