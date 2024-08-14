0b25528e7297bfffb6c78b68adac2d968ba00477
peng-zhou <peterzhou.syd@gmail.com>
2024-08-14 16:33:06 +1000

    remove string injection

--- widgets/quadrant/components/expanded-tile/tile.template.tsx
+++ widgets/quadrant/components/expanded-tile/tile.template.tsx
@@ -2,94 +2,104 @@ import type { Sdk } from "@stackla/ugc-widgets"
 import { Tile } from "@stackla/ugc-widgets"
 import { IWidgetSettings } from "../../../../types/IWidgetSettings"
 import { getTimephrase } from "../../../libs/tile.lib"
-import { createElement } from "jsx-html"
+import { createElement, createFragment } from "jsx-html"
 
 export const tileTemplate = (sdk: Sdk, widgetSettings: IWidgetSettings, tile: Tile) => {
     const shopspotEnabled = sdk.isComponentLoaded("shopspots") && widgetSettings.expanded_tile_show_shopspots
     const productsEnabled = sdk.isComponentLoaded("products") && widgetSettings.expanded_tile_show_products
     const parent = sdk.getNodeId()
-    return <div class="panel">
-            <a class="exit" href="#">
-                <span class="widget-icon close-white"></span>
+    return (
+        <div className="panel">
+            <a className="exit" href="#">
+                <span className="widget-icon close-white"></span>
             </a>
-            <div class="tile-arrows">
-                <button class="tile-arrows-btn tile-arrows-left"><span class="widget-icon chevron-left"></span></button>
-                <button class="tile-arrows-btn tile-arrows-right" ><span class="widget-icon chevron-right"></span></button>
+            <div className="tile-arrows">
+                <button className="tile-arrows-btn tile-arrows-left">
+                    <span className="widget-icon chevron-left"></span>
+                </button>
+                <button className="tile-arrows-btn tile-arrows-right">
+                    <span className="widget-icon chevron-right"></span>
+                </button>
             </div>
-            <div class="panel-left">
-                <div class="image-wrapper">
-                    <div class="image-wrapper-inner">
-                        <div class="image">
-                            ${shopspotEnabled ? `<shopspot-flyout parent="${parent}"></shopspot-flyout>` : ""}
-                            ${shopspotEnabled ? `<shopspot-icon parent="${parent}"/></shopspot-icon>` : ""}
-                            ${tile.image ? `<img class="image-element" src="${tile.image}" />` : ""}
+            <div className="panel-left">
+                <div className="image-wrapper">
+                    <div className="image-wrapper-inner">
+                        <div className="image">
+                            {shopspotEnabled ? <shopspot-flyout parent={parent}></shopspot-flyout> : null}
+                            {shopspotEnabled ? <shopspot-icon parent={parent}/> : null}
+                            {tile.image ? <img className="image-element" src={tile.image} /> : null}
                         </div>
                     </div>
                 </div>
                 <div>
-                    <span class="source">
-                        <i class="fs fs-${tile.source}"></i>
+                    <span className="source">
+                        <i className={`fs fs-${tile.source}`}></i>
                     </span>
                 </div>
             </div>
-            <div class="panel-right">
-                <div class="panel-right-wrapper">
-                    <div class="content-wrapper">
-                        <div class="content-inner-wrapper">
-                            <div class="user-info-wrapper">
-                                <div class="user-info">
-                                    ${
-                                      tile.avatar
-                                        ? `<span class="avatar-wrapper">
-                                        <a class="avatar-link" href="${tile.original_url}" target="_blank">
-                                            <img src="${tile.avatar}">
+            <div className="panel-right">
+                <div className="panel-right-wrapper">
+                    <div className="content-wrapper">
+                        <div className="content-inner-wrapper">
+                            <div className="user-info-wrapper">
+                                <div className="user-info">
+                                    {tile.avatar && (
+                                        <span className="avatar-wrapper">
+                                            <a className="avatar-link" href={tile.original_url} target="_blank" rel="noopener noreferrer">
+                                                <img src={tile.avatar} alt="User Avatar"/>
+                                            </a>
+                                        </span>
+                                    )}
+                                    {tile.user && (
+                                        <a className="user-link" href={tile.original_url} target="_blank" rel="noopener noreferrer">
+                                            <div className="user-top">
+                                                <span className="user-name">{tile.user}</span>
+                                            </div>
+                                            <div className="user-bottom">
+                                                <span className="user-handle">@{tile.user}</span>
+                                            </div>
                                         </a>
-                                    </span>`
-                                        : ""
-                                    }
-                                    ${
-                                      tile.user
-                                        ? `<a class="user-link" href="${tile.original_url}" target="_blank">
-                                        <div class="user-top">
-                                            <span class="user-name">${tile.user}</span>
-                                        </div>
-                                        <div class="user-bottom">
-                                            <span class="user-handle">@${tile.user}</span>
-                                        </div>
-                                    </a>`
-                                        : ""
-                                    }
+                                    )}
                                 </div>
                             </div>
-                            <div class="tile-timestamp">${tile.source_created_at && widgetSettings.expanded_tile_show_timestamp ? getTimephrase(tile.source_created_at) : ""}</div>
-                            <div class="caption">
-                                <p class="caption-paragraph">${
-                                  tile.message && widgetSettings.expanded_tile_show_caption ? tile.message : ""
-                                }</p>
-                                ${
-                                  widgetSettings.expanded_tile_show_sharing
-                                    ? `<!-- AddToAny BEGIN -->
-                                   <div class="ugc-inline-share-buttons">
-                                     <a href="https://www.addtoany.com/add_to/facebook?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/facebook.svg" width="32" height="32" style="background-color:#333"></a>
-                                     <a href="https://www.addtoany.com/add_to/x?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/x.svg" width="32" height="32" style="background-color:#333"></a>
-                                     <a href="https://www.addtoany.com/add_to/pinterest?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/pinterest.svg" width="32" height="32" style="background-color:#333"></a>
-                                     <a href="https://www.addtoany.com/add_to/linkedin?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/linkedin.svg" width="32" height="32" style="background-color:#333"></a>
-                                     <a href="https://www.addtoany.com/add_to/email?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/email.svg" width="32" height="32" style="background-color:#333"></a>
-                                   </div>
-                                   <!-- AddToAny END -->`
-                                    : ""
-                                }
-                                ${productsEnabled ? `<ugc-products parent="${parent}">` : ""}
+                            <div className="tile-timestamp">
+                                {tile.source_created_at && widgetSettings.expanded_tile_show_timestamp ? getTimephrase(tile.source_created_at) : ""}
                             </div>
-                            <div class="sharethis-inline-share-buttons"></div>
-                            <div class="footer">
-                                <span class="base-v2 source source-instagram">
-                                    <i class="fs fs-instagram"></i>
+                            <div className="caption">
+                                <p className="caption-paragraph">
+                                    {tile.message && widgetSettings.expanded_tile_show_caption ? tile.message : ""}
+                                </p>
+                                {widgetSettings.expanded_tile_show_sharing && (
+                                    <div className="ugc-inline-share-buttons">
+                                        <a href={`https://www.addtoany.com/add_to/facebook?linkurl=${tile.original_url}&linkname=${tile.name}`} target="_blank" rel="noopener noreferrer">
+                                            <img src="https://static.addtoany.com/buttons/facebook.svg" width="32" height="32" style={{backgroundColor:"#333"}} alt="Share on Facebook"/>
+                                        </a>
+                                        <a href={`https://www.addtoany.com/add_to/x?linkurl=${tile.original_url}&linkname=${tile.name}`} target="_blank" rel="noopener noreferrer">
+                                            <img src="https://static.addtoany.com/buttons/x.svg" width="32" height="32" style={{backgroundColor:"#333"}} alt="Share on X"/>
+                                        </a>
+                                        <a href={`https://www.addtoany.com/add_to/pinterest?linkurl=${tile.original_url}&linkname=${tile.name}`} target="_blank" rel="noopener noreferrer">
+                                            <img src="https://static.addtoany.com/buttons/pinterest.svg" width="32" height="32" style={{backgroundColor:"#333"}} alt="Share on Pinterest"/>
+                                        </a>
+                                        <a href={`https://www.addtoany.com/add_to/linkedin?linkurl=${tile.original_url}&linkname=${tile.name}`} target="_blank" rel="noopener noreferrer">
+                                            <img src="https://static.addtoany.com/buttons/linkedin.svg" width="32" height="32" style={{backgroundColor:"#333"}} alt="Share on LinkedIn"/>
+                                        </a>
+                                        <a href={`https://www.addtoany.com/add_to/email?linkurl=${tile.original_url}&linkname=${tile.name}`} target="_blank" rel="noopener noreferrer">
+                                            <img src="https://static.addtoany.com/buttons/email.svg" width="32" height="32" style={{backgroundColor:"#333"}} alt="Share via Email"/>
+                                        </a>
+                                    </div>
+                                )}
+                                {productsEnabled && <ugc-products parent={parent}></ugc-products>}
+                            </div>
+                            <div className="sharethis-inline-share-buttons"></div>
+                            <div className="footer">
+                                <span className="base-v2 source source-instagram">
+                                    <i className="fs fs-instagram"></i>
                                 </span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-          </div>
+        </div>
+    )
 }
