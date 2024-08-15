3a9e36787f50901d4b340c62e1674b869269d382
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-08-16 09:13:24 +1000

    Add support for timephrase in waterfall

---
+++ cypress/e2e/widget/waterfall.cy.js
@@ -0,0 +1,14 @@
+describe('Should test the carousel', () => {
+    it('Should pass default tests', () => {
+        cy.widgetTests('carousel');
+    })
+
+    it('Should display timephrase in the tile', () => {
+        cy.visitWidget('carousel');
+
+        cy.getFirstTile().find('.tile-timephrase')
+        .should('exist')
+        .invoke('text')
+        .should('not.be.empty');
+    })
+})
