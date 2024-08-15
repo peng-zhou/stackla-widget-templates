c8e8d4b32664bd042353ecfee642069358aeb7a6
Manikandan Ravikumar <manikandan.ravikumar@nosto.com>
2024-08-15 17:07:34 +0300

    improvements, fixes and addressing comments

--- widgets/carousel/components/expanded-tile/tile.template.tsx
+++ widgets/carousel/components/expanded-tile/tile.template.tsx
@@ -1,8 +1,9 @@
 import type { Sdk } from "@stackla/ugc-widgets"
 import { Tile } from "@stackla/ugc-widgets"
 import { IWidgetSettings } from "types/IWidgetSettings"
-import { getTimephrase } from "../../../libs/tile.lib"
-import { createElement } from "jsx-html"
+import { getTimephrase } from "@libs/tile.lib"
+import { createElement, createFragment } from "jsx-html"
+import { getTagsFromTile } from "@libs/templates/expanded-tile.lib"
 
 export const tileTemplate = (sdk: Sdk, widgetSettings: IWidgetSettings, tile: Tile) => {
   const shopspotEnabled = sdk.isComponentLoaded("shopspots") && widgetSettings.expanded_tile_show_shopspots
@@ -19,15 +20,14 @@ export const tileTemplate = (sdk: Sdk, widgetSettings: IWidgetSettings, tile: Ti
           <div class="image-wrapper-inner">
             <div class="image-filler" style="background-image: url(${tile.image})"></div>
             <div class="image">
-              ${shopspotEnabled ? `<shopspot-flyout parent="${parent}"></shopspot-flyout>` : ""}$
-              {shopspotEnabled ? `<shopspot-icon parent="${parent}"/></shopspot-icon>` : ""}$
-              {tile.image ? `<img class="image-element" src="${tile.image}" />` : ""}
+              {shopSpotTemplate(shopspotEnabled, parent)}
+              {tile.image ? <img class="image-element" src={tile.image} /> : ""}
             </div>
           </div>
         </div>
         <div>
           <span class="source">
-            <i class="fs fs-${tile.source}"></i>
+            <i class={"fs fs-" + tile.source}></i>
           </span>
         </div>
       </div>
@@ -36,51 +36,20 @@ export const tileTemplate = (sdk: Sdk, widgetSettings: IWidgetSettings, tile: Ti
           <div class="content-wrapper">
             <div class="content-inner-wrapper">
               <div class="user-info-wrapper">
-                <div class="user-info">
-                  $
-                  {tile.avatar
-                    ? `<span class="avatar-wrapper">
-                                        <a class="avatar-link" href="${tile.original_url}" target="_blank">
-                                            <img src="${tile.avatar}">
-                                        </a>
-                                    </span>`
-                    : ""}
-                  $
-                  {tile.user
-                    ? `<a class="user-link" href="${tile.original_url}" target="_blank">
-                                        <div class="user-top">
-                                            <span class="user-name">${tile.user}</span>
-                                        </div>
-                                        <div class="user-bottom">
-                                            <span class="user-handle">@${tile.user}</span>
-                                        </div>
-                                    </a>`
-                    : ""}
-                </div>
+                <div class="user-info">${userInfoTemplate(tile)}</div>
               </div>
               <div class="tile-timestamp">
-                $
                 {tile.source_created_at && widgetSettings.expanded_tile_show_timestamp
                   ? getTimephrase(tile.source_created_at)
                   : ""}
               </div>
               <div class="caption">
                 <p class="caption-paragraph">
-                  ${tile.message && widgetSettings.expanded_tile_show_caption ? tile.message : ""}
+                  {tile.message && widgetSettings.expanded_tile_show_caption ? tile.message : ""}
                 </p>
-                $
-                {widgetSettings.expanded_tile_show_sharing
-                  ? `<div class="ugc-inline-share-buttons">
-                                    <a href="https://www.addtoany.com/add_to/facebook?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/facebook.svg" width="32" height="32" style="background-color:#333"></a>
-                                    <a href="https://www.addtoany.com/add_to/x?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/x.svg" width="32" height="32" style="background-color:#333"></a>
-                                    <a href="https://www.addtoany.com/add_to/pinterest?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/pinterest.svg" width="32" height="32" style="background-color:#333"></a>
-                                    <a href="https://www.addtoany.com/add_to/linkedin?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/linkedin.svg" width="32" height="32" style="background-color:#333"></a>
-                                    <a href="https://www.addtoany.com/add_to/email?linkurl=${tile.original_url}&amp;linkname=${tile.name}" target="_blank"><img src="https://static.addtoany.com/buttons/email.svg" width="32" height="32" style="background-color:#333"></a>
-                                </div>`
-                  : ""}
-                ${productsEnabled ? `<ugc-products parent="${parent}">` : ""}
+                {getTagsFromTile(tile)}
+                {productsEnabled ? <ugc-products parent={parent} /> : ""}
               </div>
-              <div class="sharethis-inline-share-buttons"></div>
               <div class="footer">
                 <span class="base-v2 source source-instagram">
                   <i class="fs fs-instagram"></i>
@@ -92,4 +61,93 @@ export const tileTemplate = (sdk: Sdk, widgetSettings: IWidgetSettings, tile: Ti
       </div>
     </div>
   )
+}
+
+const userInfoTemplate = (tile: Tile) => {
+  if (tile.avatar) {
+    return (
+      <span class="avatar-wrapper">
+        <a class="avatar-link" href={tile.original_url} target="_blank">
+          <img src={tile.avatar} />
+        </a>
+      </span>
+    )
+  }
+  if (tile.user) {
+    return (
+      <a class="user-link" href={tile.original_url} target="_blank">
+        <div class="user-top">
+          <span class="user-name">{tile.user}</span>
+        </div>
+        <div class="user-bottom">
+          <span class="user-handle">@{tile.user}</span>
+        </div>
+      </a>
+    )
+  }
+  return ""
+}
+
+const shopSpotTemplate = (shopspotEnabled: boolean, parent?: string) => {
+  if (shopspotEnabled) {
+    return (
+      <>
+        <shopspot-flyout parent={parent}></shopspot-flyout>
+        <shopspot-icon parent={parent} />
+      </>
+    )
+  }
+  return ""
+}
+
+const shareMenuTemplate = (tile: Tile, showMenu: boolean) => {
+  const constructUrl = (icon: string) => {
+    const url = new URL(`https://www.addtoany.com/add_to/${icon}`)
+    url.searchParams.append("linkurl", tile.original_url)
+    tile.name && url.searchParams.append("linkname", tile.name)
+    return url.href
+  }
+
+  if (showMenu) {
+    return (
+      <div class="ugc-inline-share-buttons">
+        <a href={constructUrl("facebook")} target="_blank">
+          <img
+            src="https://static.addtoany.com/buttons/facebook.svg"
+            width="32"
+            height="32"
+            style="background-color:#333"
+          />
+        </a>
+        <a href={constructUrl("x")} target="_blank">
+          <img src="https://static.addtoany.com/buttons/x.svg" width="32" height="32" style="background-color:#333" />
+        </a>
+        <a href={constructUrl("pinterest")} target="_blank">
+          <img
+            src="https://static.addtoany.com/buttons/pinterest.svg"
+            width="32"
+            height="32"
+            style="background-color:#333"
+          />
+        </a>
+        <a href={constructUrl("linkedin")} target="_blank">
+          <img
+            src="https://static.addtoany.com/buttons/linkedin.svg"
+            width="32"
+            height="32"
+            style="background-color:#333"
+          />
+        </a>
+        <a href={constructUrl("email")} target="_blank">
+          <img
+            src="https://static.addtoany.com/buttons/email.svg"
+            width="32"
+            height="32"
+            style="background-color:#333"
+          />
+        </a>
+      </div>
+    )
+  }
+  return ""
 }
