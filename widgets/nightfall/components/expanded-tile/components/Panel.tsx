aeb23038acc7eed76a761db29f817a6ec149123d
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-08-20 13:12:25 +1000

    Fixup type

--- widgets/nightfall/components/expanded-tile/components/Panel.tsx
+++ widgets/nightfall/components/expanded-tile/components/Panel.tsx
@@ -2,7 +2,7 @@ import CloseIcon from "./CloseIcon"
 import TileArrows from "./TileArrows"
 import { createElement } from "@stackla/ugc-widgets/src/ui/core/utils/jsx-html"
 
-export default (props : { children?: JSX.Element[]}) => (
+export default (props: { children?: JSX.Element[] }) => (
   <div className="panel">
     <CloseIcon />
     <TileArrows />
