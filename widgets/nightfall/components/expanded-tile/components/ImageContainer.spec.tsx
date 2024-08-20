34016641da7be4fd45ed19aaed77f20242f67ee2
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-08-20 15:22:09 +1000

    Update image container spec

--- widgets/nightfall/components/expanded-tile/components/ImageContainer.spec.tsx
+++ widgets/nightfall/components/expanded-tile/components/ImageContainer.spec.tsx
@@ -26,20 +26,20 @@ describe("Test shopspot logic", () => {
     const shopspots = getShopspots(widgetSettings)
 
     expect(shopspots).not.toBeInstanceOf(DocumentFragment)
-  });
+  })
 
   test("returns empty fragment when shopspot is disabled", () => {
     const widgetSettings = { expanded_tile_show_shopspots: false } as IWidgetSettings
     const shopspots = getShopspots(widgetSettings)
 
     expect(shopspots).toBeInstanceOf(DocumentFragment)
-  });
+  })
 })
 
 describe("ImageContainer", () => {
   beforeEach(() => {
     document.body.innerHTML = ""
-  });
+  })
 
   test("renders image element when tile has image", () => {
     const widgetSettings = { expanded_tile_show_shopspots: true } as IWidgetSettings
@@ -48,20 +48,20 @@ describe("ImageContainer", () => {
     document.body.appendChild(imageContainer)
 
     expect(document.body.innerHTML).toBe(
-      `<div class=\"image-wrapper\"><div class=\"image-wrapper-inner\"><div class=\"image\"><div><shopspot-flyout parent=\"undefined\"></shopspot-flyout><shopspot-icon parent=\"undefined\"></shopspot-icon></div><img alt=\"UGC Image\" class=\"image-element\" src=\"https://media-library.stackla.com/20/fixtures/2024-06/666f8566f7e6e366ff94950c/e0ef24c0-2c41-11ef-af7f-2b848b315e63.jpg?format=webp\"></div></div></div><div><span class=\"source\"><i class=\"fs fs-instagram\"></i></span></div>`
-    );
-  });
+      `<div class="image-wrapper"><div class="image-wrapper-inner"><div class="image"><div><shopspot-flyout parent="undefined"></shopspot-flyout><shopspot-icon parent="undefined"></shopspot-icon></div><img alt="UGC Image" class="image-element" src="https://media-library.stackla.com/20/fixtures/2024-06/666f8566f7e6e366ff94950c/e0ef24c0-2c41-11ef-af7f-2b848b315e63.jpg?format=webp"></div></div></div><div><span class="source"><i class="fs fs-instagram"></i></span></div>`
+    )
+  })
 
-  it('renders shopspot components when shopspot is enabled', () => {
+  it("renders shopspot components when shopspot is enabled", () => {
     const widgetSettings = { expanded_tile_show_shopspots: true } as IWidgetSettings
     const imageContainer = <ImageContainer tile={tiles[0]} widgetSettings={widgetSettings} />
-    
+
     mockSdk.isComponentLoaded.mockReturnValue(true)
 
     document.body.appendChild(imageContainer)
 
-    expect(document.body.querySelector("shopspot-flyout")).not.toBeNull();
-  });
+    expect(document.body.querySelector("shopspot-flyout")).not.toBeNull()
+  })
 
   it("does not render shopspot components when shopspot is disabled", () => {
     const widgetSettings = { expanded_tile_show_shopspots: false } as IWidgetSettings
@@ -71,6 +71,6 @@ describe("ImageContainer", () => {
 
     document.body.appendChild(imageContainer)
 
-    expect(document.body.querySelector("shopspot-flyout")).toBeNull();
-  });
-});
+    expect(document.body.querySelector("shopspot-flyout")).toBeNull()
+  })
+})
