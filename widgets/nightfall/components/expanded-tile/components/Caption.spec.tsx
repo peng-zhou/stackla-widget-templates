c18bafe23b7efab41367b620f1615adb8cbeb892
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-08-23 09:03:45 +1000

    Fixup tests

--- widgets/nightfall/components/expanded-tile/components/Caption.spec.tsx
+++ widgets/nightfall/components/expanded-tile/components/Caption.spec.tsx
@@ -6,7 +6,20 @@ import { IWidgetSettings } from "types/IWidgetSettings"
 
 const mockSdk = {
   isComponentLoaded: jest.fn(),
-  getNodeId: jest.fn()
+  getNodeId: jest.fn(),
+  placement: {
+    getWidgetContainer: () => {
+      return {
+        widgetOptions: {
+          widgetStyle: {
+            expanded_tile_show_caption: false
+          },
+          widgetConfig: {}
+        },
+        enabled: true
+      }
+    }
+  },
 }
 
 // @ts-expect-error global properties are not typed
@@ -52,7 +65,7 @@ describe("Caption Component", () => {
   it("should render vital components", () => {
     const caption = <Caption tile={tile} widgetSettings={widgetSettings} />
     expect(caption.toString()).toBe(
-      `<div class="caption"><p class="caption-paragraph">Sample caption</p><ugc-products parent="parent-id"></ugc-products></div>`
+      `<div class="caption"><p class="caption-paragraph">Sample caption</p><div class="tile-timestamp"></div><ugc-products parent="parent-id"></ugc-products></div>`
     )
   })
 
@@ -60,7 +73,7 @@ describe("Caption Component", () => {
     const widgetSettings = { expanded_tile_show_caption: false } as IWidgetSettings
     const caption = <Caption tile={tile} widgetSettings={widgetSettings} />
     expect(caption.toString()).toBe(
-      `<div class="caption"><p class="caption-paragraph"></p><ugc-products parent="parent-id"></ugc-products></div>`
+      `<div class="caption"><p class="caption-paragraph"></p><div class="tile-timestamp"></div><ugc-products parent="parent-id"></ugc-products></div>`
     )
   })
 
@@ -68,13 +81,13 @@ describe("Caption Component", () => {
     const tile = tiles[0]
     const caption = <Caption tile={tile} widgetSettings={widgetSettings} />
     expect(caption.toString()).toContain(
-      `<div class="caption"><p class="caption-paragraph"></p><div class="tags"><div class="tag"><a href="#">Double column span</a></div><div class="tag"><a href="#">Single column span</a></div><div class="tag"><a href="#">auto_twitter_user</a></div><div class="tag"><a href="https://www.topshop.com/webapp/wcs/stores/servlet/ProductDisplay?catalogId=33057&storeId=12556&productId=33834478&langId=-1&cmpId=615&viewAllFlag=false&sort_field=Relevance&beginIndex=1&pageSize=20&categoryId=3497014&parent_categoryId=204484">**Embroidered Watch</a></div><div class="tag"><a href="https://visual-ugc-staging.myshopify.com/products/lime-cardigannosto=ugc-widget-recommendation-1">Lime Cardigan</a></div><div class="tag"><a href="https://visual-ugc-staging.myshopify.com/products/sleeveless-cape-jacket-blacknosto=ugc-widget-recommendation-1">Sleeveless Cape Jacket</a></div><div class="tag"><a href="https://visual-ugc-staging.myshopify.com/products/s14-onl-li-5656-blacknosto=ugc-widget-recommendation-1">Delicious Camisole</a></div></div><ugc-products parent="parent-id"></ugc-products></div>`
+      `<div class="caption"><p class="caption-paragraph"></p><div class="tile-timestamp"></div><div class="tags"><div class="tag"><a href="#">Double column span</a></div><div class="tag"><a href="#">Single column span</a></div><div class="tag"><a href="#">auto_twitter_user</a></div><div class="tag"><a href="https://www.topshop.com/webapp/wcs/stores/servlet/ProductDisplay?catalogId=33057&storeId=12556&productId=33834478&langId=-1&cmpId=615&viewAllFlag=false&sort_field=Relevance&beginIndex=1&pageSize=20&categoryId=3497014&parent_categoryId=204484">**Embroidered Watch</a></div><div class="tag"><a href="https://visual-ugc-staging.myshopify.com/products/lime-cardigannosto=ugc-widget-recommendation-1">Lime Cardigan</a></div><div class="tag"><a href="https://visual-ugc-staging.myshopify.com/products/sleeveless-cape-jacket-blacknosto=ugc-widget-recommendation-1">Sleeveless Cape Jacket</a></div><div class="tag"><a href="https://visual-ugc-staging.myshopify.com/products/s14-onl-li-5656-blacknosto=ugc-widget-recommendation-1">Delicious Camisole</a></div></div><ugc-products parent="parent-id"></ugc-products></div>`
     )
   })
 
   it("should not render ugc-products component when products are disabled", () => {
     mockSdk.isComponentLoaded.mockReturnValue(false)
     const caption = <Caption tile={tile} widgetSettings={widgetSettings} />
-    expect(caption.toString()).toBe(`<div class="caption"><p class="caption-paragraph">Sample caption</p></div>`)
+    expect(caption.toString()).toBe(`<div class="caption"><p class="caption-paragraph">Sample caption</p><div class="tile-timestamp"></div></div>`)
   })
 })
