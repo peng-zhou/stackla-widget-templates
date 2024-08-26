4dda06529f84133ac1bee0b2faa8a2e10bf758e2
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-08-26 17:07:59 +1000

    Add timeout

--- widgets/nightfall/widget.listeners.ts
+++ widgets/nightfall/widget.listeners.ts
@@ -4,7 +4,7 @@ import { waitForElement } from "widgets/libs/widget.utils"
 declare const sdk: Sdk
 
 export async function onTileClose() {
-  await waitForElement(".ugc-tiles")
+  await waitForElement(".ugc-tiles", 3000)
   const tilesContainer = sdk.querySelector("#tiles")
   const ugcTiles = sdk.querySelector(".ugc-tiles")
 
