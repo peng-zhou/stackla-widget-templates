5e2cbf6d265a1ddd9e91b0ebc43ac08e0f5d2690
Manikandan Ravikumar <manikandan.ravikumar@nosto.com>
2024-09-12 08:14:32 +0300

    remove unnecessary changes

--- widgets/carousel/widget.ts
+++ widgets/carousel/widget.ts
@@ -1,7 +1,5 @@
 import { loadCustomisation } from "./widget.templates"
 import { loadSettings } from "./widget.settings"
-import { Sdk } from "@stackla/ugc-widgets"
 
-declare const sdk: Sdk
 loadSettings()
 loadCustomisation()
