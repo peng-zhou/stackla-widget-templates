569e2904802e38dac09d77a17c0b2ab9f31fc1af
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-09-13 15:07:53 +1000

    Refactored

--- widgets/nightfall/widget.listeners.ts
+++ widgets/nightfall/widget.listeners.ts
@@ -5,18 +5,13 @@ declare const sdk: Sdk
 
 export async function onTileClose() {
   await waitForElement(".ugc-tiles", 3000)
-  const tilesContainer = sdk.querySelector("#tiles")
   const ugcTiles = sdk.querySelector(".ugc-tiles")
 
   if (!ugcTiles) {
     throw new Error("Failed to find tiles wrapper")
   }
 
-  if (!tilesContainer) {
-    throw new Error("Failed to find tiles container")
-  }
-
-  tilesContainer.style.display = "flex"
+  ugcTiles.style.display = "flex"
 
   refreshMasonryLayout()
 }
