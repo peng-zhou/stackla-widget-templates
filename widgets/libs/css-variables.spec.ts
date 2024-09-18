d1aa510ed1e81eed9f6133a2340fdff3e83503e4
aistetamos <aiste@searchnode.com>
2024-09-18 13:31:34 +0300

    update test

--- widgets/libs/css-variables.spec.ts
+++ widgets/libs/css-variables.spec.ts
@@ -48,6 +48,7 @@ describe("getCSSVariables", () => {
     --show-caption:block;
     --tile-timephrase-display:none;
     --shopspot-icon:url("http://example.com/icon.png");
+    --tags-gap:4px;
       `
 
     expect(getCSSVariables(widgetSettings).replace(/\s/g, "")).toBe(expectedCSS.replace(/\s/g, ""))
@@ -94,6 +95,7 @@ describe("getCSSVariables", () => {
     --show-caption:none;
     --tile-timephrase-display:inline-block;
     --shopspot-icon:url("data:image/svg+xml,%3Csvgxmlns='http://www.w3.org/2000/svg'viewBox='00512512'%3E%3Cpathd='M34539.1L472.8168.4c52.45352.4138.20191.2L360.8472.9c-9.39.4-24.59.5-33.9.2s-9.5-24.5-.2-33.9L438.6325.9c33.9-34.333.9-89.40-123.7L310.972.9c-9.3-9.4-9.2-24.6.2-33.9s24.6-9.233.9.2zM0229.5V80C053.521.5324832H197.5c17033.36.745.318.7l168168c25252565.5090.5L277.3442.7c-2525-65.525-90.50l-168-168C6.7262.70246.50229.5zM144144a3232010-6403232010640z'fill='%23fff'/%3E%3C/svg%3E");
+    --tags-gap:4px;
       `
 
     expect(getCSSVariables(widgetSettings).replace(/\s/g, "")).toBe(expectedCSS.replace(/\s/g, ""))
