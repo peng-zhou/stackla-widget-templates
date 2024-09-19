e36f71fe5ed914ad1530dae37762a4c058e171ea
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-09-19 17:29:22 +1000

    Cleanup

--- src/libs/static-files.ts
+++ src/libs/static-files.ts
@@ -3,28 +3,25 @@ import path from "path"
 
 export const WIDGET_PATH = "../../../../../node_modules/@stackla/ugc-widgets/dist"
 
+function getComponentFilePaths() {
+  const components = ['core', 'expanded-tile', 'shopspots', 'google-analytics', 'cross-sellers', 'add-to-cart', 'products'];
+
+  return components.map(component => path.resolve(__dirname, `${WIDGET_PATH}/${component}.js`));
+}
+
 export function loadStaticFileRoutes(expressApp) {
-  const serveStaticFile = (route, fileName, contentType = "text/javascript") => {
-    expressApp.get(route, (req, res) => {
-      const code = readFileSync(path.resolve(__dirname, `${WIDGET_PATH}/${fileName}`), "utf8")
-      res.set("Content-Type", contentType)
-      res.send(code)
+  getComponentFilePaths().forEach((componentPath) => {
+    const jsCode = fs.readFileSync(componentPath, "utf8")
+    const componentName = path.basename(componentPath, '.js')
+
+    expressApp.get(`/${componentName}.js`, (req, res) => {
+      const parsedCode = jsCode
+        .replace(/https:\/\/widget-data.stackla.com/g, "http://localhost:4003")
+        .replace(/https:\/\/widget-ui.stackla.com/g, "http://localhost:4003")
+        .replace(/\/tiles\?/g, `/tiles?widgetType=${req.query.widgetType}&`)
+      
+      res.setHeader("Content-Type", "application/javascript")
+      res.send(parsedCode)
     })
-  }
-
-  serveStaticFile("/expanded-tile.js", "expanded-tile.js")
-  serveStaticFile("/shopspots.js", "shopspots.js")
-  serveStaticFile("/google-analytics.js", "google-analytics.js")
-  serveStaticFile("/cross-sellers.js", "cross-sellers.js")
-  serveStaticFile("/add-to-cart.js", "add-to-cart.js")
-  serveStaticFile("/products.js", "products.js")
-
-  expressApp.get("/core.js", (req, res) => {
-    const jsCode = fs.readFileSync(path.resolve(__dirname, `${WIDGET_PATH}/core.js`), "utf8")
-    const parsedCode = jsCode
-      .replace(/https:\/\/widget-data.stackla.com/g, "http://localhost:4003")
-      .replace(/https:\/\/widget-ui.stackla.com/g, "http://localhost:4003")
-      .replace(/\/tiles\?/g, `/tiles?widgetType=${req.query.widgetType}&`)
-    res.send(parsedCode)
-  })
+  });
 }
