8ad75e87dd721d6298119feaa086abe92a979ea1
aistetamos <aiste@searchnode.com>
2024-10-10 13:34:48 +0300

    fix types

--- types/IWidgetSettings.ts
+++ types/IWidgetSettings.ts
@@ -37,6 +37,6 @@ export interface IWidgetSettings extends BaseConfig {
   expanded_tile_show_sharing: boolean
   expanded_tile_show_caption: boolean
   expanded_tile_show_timestamp: boolean
-  expanded_tile_show_tags: boolean
-  expanded_tile_show_navigation_arrows: boolean
+  expanded_tile_show_tags?: boolean
+  expanded_tile_show_navigation_arrows?: boolean
 }
