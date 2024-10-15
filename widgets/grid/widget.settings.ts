695609c18d2f472b343e5ec3a2bc2a48680db20b
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-10-15 15:17:18 +1100

    Rebase
    Signed-off-by: Oliver Kucharzewski <oliver.kucharzewski@nosto.com>

---
+++ widgets/grid/widget.settings.ts
@@ -0,0 +1,42 @@
+import { Sdk } from "@stackla/ugc-widgets"
+import getCSSVariables from "@widgets/libs/css-variables"
+import { MasonryLayout } from "@appnest/masonry-layout"
+import {
+  loadTitle,
+  addAutoAddTileFeature,
+  loadExpandedTileFeature,
+  addTilesPerPageFeature,
+  addLoadMoreButtonFeature
+} from "@widgets/libs/widget.features"
+import { addCSSVariablesToPlacement } from "@widgets/libs/widget.layout"
+import { getConfig } from "./widget.config"
+import { onTileExpand, onTileClosed, onTileRendered } from "@widgets/libs/extensions/swiper/swiper.expanded-tile"
+
+declare const sdk: Sdk
+
+export function loadWidgetSettings() {
+  const widgetContainer = sdk.placement.getWidgetContainer()
+  const widgetSettings = getConfig(widgetContainer)
+
+  loadTitle()
+  addCSSVariablesToPlacement(getCSSVariables(widgetSettings))
+  addAutoAddTileFeature(widgetSettings)
+  loadExpandedTileFeature(widgetSettings, onTileExpand, onTileClosed, onTileRendered)
+  addTilesPerPageFeature(widgetSettings)
+  addLoadMoreButtonFeature(widgetSettings)
+
+  const refreshMasonryLayout = () => {
+    const masonryLayout = sdk.querySelector<MasonryLayout>("masonry-layout")
+    if (masonryLayout instanceof MasonryLayout) {
+      masonryLayout.layout()
+    }
+  }
+
+  sdk.addEventListener("moreLoad", () => {
+    refreshMasonryLayout()
+  })
+  window.addEventListener("scroll", () => {
+    refreshMasonryLayout()
+  })
+  sdk.addEventListener("tilesUpdated", refreshMasonryLayout)
+}
---
+++ widgets/grid/widget.ts
@@ -0,0 +1,7 @@
+import { loadAllUnloadedTiles } from "@widgets/libs/extensions/swiper/loader.extension"
+import { loadWidgetSettings } from "./widget.settings"
+import { loadCustomisation } from "./widget.templates"
+
+loadCustomisation()
+loadWidgetSettings()
+loadAllUnloadedTiles()
