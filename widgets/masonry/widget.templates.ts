25e3c0d465c10d92581c836d45a31f4e23de1d16
Oliver Kucharzewski <107518060+olivernosto@users.noreply.github.com>
2024-10-22 15:14:07 +1100

    Update widget.templates.ts

--- widgets/masonry/widget.templates.ts
+++ widgets/masonry/widget.templates.ts
@@ -1,7 +1,5 @@
 import { loadExpandedTileTemplates } from "@libs/components/expanded-tile-swiper"
 import { loadShopspotTemplates } from "./components/shopspot-icon"
-import { Sdk } from "@stackla/ugc-widgets"
-declare const sdk: Sdk
 
 export function loadCustomisation() {
   loadExpandedTileTemplates()
