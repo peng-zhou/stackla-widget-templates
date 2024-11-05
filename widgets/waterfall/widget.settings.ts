26317983ee34670833e01b5989f2e57ef5ee9dff
aistetamos <aiste@searchnode.com>
2024-11-05 09:55:02 +0200

    add missing files after master merge

--- widgets/waterfall/widget.settings.ts
+++ widgets/waterfall/widget.settings.ts
@@ -1,47 +1,28 @@
 import { Sdk } from "@stackla/ugc-widgets"
-import getCSSVariables from "@widgets/libs/css-variables"
-import {
-  loadTitle,
-  addAutoAddTileFeature,
-  loadExpandedTileFeature,
-  addTilesPerPageFeature,
-  addLoadMoreButtonFeature
-} from "@widgets/libs/widget.features"
-import { addCSSVariablesToPlacement } from "@widgets/libs/widget.layout"
-import { getConfig } from "./widget.config"
-import {
-  onTileExpand,
-  onTileClosed,
-  onTileRendered
-} from "@libs/components/expanded-tile-swiper/expanded-swiper.loader"
+import { loadWidget } from "@stackla/widget-utils"
 import {
   refreshWaterfallLayout,
   reinitialiseWaterfallLayout,
   resizeAllUgcTilesHeight
 } from "@widgets/libs/extensions/waterfall.extension"
-import { onTagsRendered } from "@widgets/libs/components/expanded-tile-swiper/tags-swiper.loader"
-import { registerTagsRendered } from "@widgets/libs/tile.listeners"
 
 declare const sdk: Sdk
 
-export async function loadWidgetSettings() {
-  const widgetContainer = sdk.placement.getWidgetContainer()
-  const widgetSettings = getConfig(widgetContainer)
+export function getSettings() {
+  return {
+    extensions: {},
+    features: {},
+    callbacks: {}
+  }
+}
 
-  loadTitle()
-  addCSSVariablesToPlacement(getCSSVariables(widgetSettings))
-  addAutoAddTileFeature(widgetSettings)
-  loadExpandedTileFeature(widgetSettings, onTileExpand, onTileClosed, onTileRendered)
-  addTilesPerPageFeature(widgetSettings)
-  addLoadMoreButtonFeature(widgetSettings)
-  registerTagsRendered(onTagsRendered)
-
-  window.refreshMasonryLayout = refreshWaterfallLayout
+export async function loadSettings() {
+  const settings = getSettings()
+  loadWidget(settings)
 
   const minmax: [number, number] = [260, 450]
 
   await resizeAllUgcTilesHeight(minmax)
-
   sdk.addEventListener("moreLoad", async () => {
     await refreshWaterfallLayout(minmax, true)
   })
---
+++ widgets/waterfall/widget.ts
@@ -0,0 +1,7 @@
+import { loadAllUnloadedTiles } from "@stackla/widget-utils/dist/libs/extensions/swiper/loader.extension"
+import { loadSettings } from "./widget.settings"
+import { loadCustomisation } from "./widget.templates"
+
+loadCustomisation()
+loadSettings()
+loadAllUnloadedTiles()
