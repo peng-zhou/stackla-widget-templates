7770a9df3885546931ba06bbe644daf90a6a0d7b
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-11-06 09:53:13 +1100

    Unify settings

--- widgets/quadrant/components/products/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ISdk } from "@stackla/widget-utils"
-import productsStyle from "./base.scss"
-
-declare const sdk: ISdk
-
-export function loadProductsTemplate() {
-  sdk.addCSSToComponent(productsStyle, "ugc-products")
-}
--- widgets/quadrant/widget.ts
+++ widgets/quadrant/widget.ts
@@ -1,6 +1,7 @@
 import { loadAllUnloadedTiles } from "@stackla/widget-utils/dist/libs/extensions/swiper/loader.extension"
 import { loadWidget } from "@stackla/widget-utils"
 import shopspotStyle from "./components/shopspot-icon/base.scss"
+import productsStyle from "./components/products/base.scss"
 
 const settings = {
   extensions: {},
@@ -14,6 +15,12 @@ const settings = {
       style: {
         css: shopspotStyle,
         global: true
+      }
+    },
+    "ugc-products": {
+      style: {
+        css: productsStyle,
+        global: false
       }
     }
   }
