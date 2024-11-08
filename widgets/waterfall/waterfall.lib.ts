975fec99d2b340901f7af84e8541492c787ec246
aistetamos <aiste@searchnode.com>
2024-11-08 13:20:00 +0200

    cleanup

--- widgets/waterfall/waterfall.lib.ts
+++ widgets/waterfall/waterfall.lib.ts
@@ -6,20 +6,9 @@ export function reinitialiseWaterfallLayout() {
   loadWaterfallLayout(true)
 }
 
-/**
- * Generate random heights for the tiles in the waterfall layout
- * @param minHeight Minimum height for each tile
- * @param maxHeight Maximum height for each tile
- * @returns
- */
-export function generateRandomHeights(minHeight: number, maxHeight: number) {
-  return Math.floor(Math.random() * (maxHeight - minHeight)) + minHeight
-}
-
 export function loadWaterfallLayout(reset = false) {
   const allTiles = Array.from(sdk.querySelectorAll<HTMLElement>(".grid-item") ?? [])
   const ugcTiles = reset ? allTiles : allTiles.filter(tile => tile.getAttribute("height-set") !== "true")
-  // const [minHeight, maxHeight] = minmax
 
   if (!ugcTiles || ugcTiles.length === 0) {
     return
