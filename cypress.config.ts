e9d0c5926d5e3b9ab32f3205104c95500edcc132
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-11-15 17:50:20 +1100

    Fixup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,22 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { configureVisualRegression } = require('cypress-visual-regression')
+import { configureVisualRegression } from "cypress-visual-regression"
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
     env: {
-      visualRegressionType: 'regression',
-      visualRegressionGenerateDiff: 'always',
+      visualRegressionType: "regression",
+      visualRegressionGenerateDiff: "always",
       visualRegressionFailSilently: false
     },
     baseUrl: "http://localhost:4002",
     supportFile: "cypress/support/e2e.ts",
     video: true,
     setupNodeEvents(on, config) {
-      
-        configureVisualRegression(on, config)
-        return config;
+      configureVisualRegression(on)
+      return config
     },
-    projectId: "2ns45q",
+    projectId: "2ns45q"
   }
 })
