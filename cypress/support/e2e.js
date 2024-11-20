d5b610fba36d24d4e49eef4ee15e14ba5ca8df3f
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2024-11-20 13:48:33 +1100

    Change to 3.

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -11,7 +11,7 @@ const WIDGET_ID = "ugc-widget-668ca52ada8fb"
 
 addCompareSnapshotCommand({
   capture: "viewport",
-  errorThreshold: 0.02
+  errorThreshold: 0.03
 })
 
 Cypress.Commands.add("before", () => {})
