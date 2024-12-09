05729307ae39206d12a643f1ee2fc56d9cf10126
aistetamos <aiste@searchnode.com>
2024-12-09 14:34:48 +0200

    adjust styles for small and large tile contents

--- widgets/waterfall/waterfall.lib.ts
+++ widgets/waterfall/waterfall.lib.ts
@@ -5,6 +5,7 @@ declare const sdk: ISdk
 export function loadWaterfallLayout(reset = false) {
   const allTiles = Array.from(sdk.querySelectorAll<HTMLElement>(".grid-item") ?? [])
   const ugcTiles = reset ? allTiles : allTiles.filter(tile => tile.getAttribute("height-set") !== "true")
+  const { inline_tile_size } = sdk.getStyleConfig()
 
   if (!ugcTiles || ugcTiles.length === 0) {
     return
@@ -17,7 +18,15 @@ export function loadWaterfallLayout(reset = false) {
   ugcTiles.forEach((tile: HTMLElement) => {
     const hasUserHandle = tile.querySelector(".user-handle") !== null
     const hasTimePhrase = tile.querySelector(".tile-timephrase") !== null
+    const bottomContainer = tile.querySelector(".tile-bottom-container") as HTMLElement
     const caption = tile.querySelector(".caption")
+    const icons = tile.querySelectorAll(".icon-share, .network-icon, .content-icon, .icon-products")
+
+    if (inline_tile_size === "small") {
+      bottomContainer.classList.add("small")
+    }
+
+    icons.forEach(icon => icon.classList.add(`${inline_tile_size}`))
 
     if (caption) {
       if (hasUserHandle || hasTimePhrase) {
