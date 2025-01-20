034303a2d94150d530a054f5599cb24c315db6a7
Oliver Kucharzewski <oliver@olidev.com.au>
2025-01-20 12:00:36 +1100

    Refactor serverless configuration and enhance Express middleware for improved environment handling

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,34 +1,7 @@
 import serverless from "serverless-http"
 import expressApp from "../../libs/express"
 
-declare const APP_ENV: string
-
-const requestModifier = async event => {
-    // Ensure headers are in array format for Lambda@Edge
-    if (event.headers) {
-      Object.keys(event.headers).forEach(key => {
-        // If the header value is not already an array, convert it into one
-        if (!Array.isArray(event.headers[key])) {
-          event.headers[key] = [{ key, value: event.headers[key] }]
-        }
-      })
-
-      console.log("Request headers", event.headers)
-    }
-
-    return event
-}
-
-const options : Record<string, unknown>= {
+const main = serverless(expressApp, {
   binary: ["image/*"]
-}
-
-if (APP_ENV === "production") {
-  options.request = requestModifier
-}
-
-console.log(APP_ENV);
-
-const main = serverless(expressApp, options);
-
+})
 export { main }
