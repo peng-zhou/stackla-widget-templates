08e6e936f368707f4244cc8abaff427f45f58d49
Oliver Kucharzewski <oliver@olidev.com.au>
2025-03-19 13:21:50 +1100

    Fix types

--- widgets/blankcanvas/widget.tsx
+++ widgets/blankcanvas/widget.tsx
@@ -13,6 +13,6 @@ sdk.querySelector("#search-input")?.addEventListener("input", async e => {
   const eventTarget = e.target
   if (eventTarget instanceof HTMLInputElement) {
     const searchValue = eventTarget.value
-    await sdk.tiles.searchTiles(searchValue)
+    await sdk.tiles.searchTiles(searchValue, true)
   }
 })
